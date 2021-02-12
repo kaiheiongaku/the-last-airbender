faf630bd63404f022d429aa6f47d92c6188b793c
Robert Heath <robert.b.heathii@gmail.com>
2021-02-12 12:28:57 -0600

    Nations Facade working

--- app/facades/nations_facade.rb
+++ app/facades/nations_facade.rb
@@ -1,13 +1,11 @@
 class NationsFacade
   class << self
-
     def characters_by_nation(nation)
-      endpoint = "/api/v1/characters?"
+      endpoint = '/api/v1/characters?'
       formatted_nation = nation.split('_').join(' ')
       params = { affiliation: formatted_nation }
       response = NationsService.call_nations_db(endpoint, params)
-      binding.pry
-      response.map { |nation_character_info| NationCharacter.new(nation_character_info) }
+      response.map { |nation_character_info| Character.new(nation_character_info) }
     end
   end
 end
