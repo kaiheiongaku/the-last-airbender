bdd624ecb5390c9811b7a8a58f8de4501410b7dc
Robert Heath <robert.b.heathii@gmail.com>
2021-02-12 11:05:18 -0600

    Buildout initial search controller and change name of search files to reflect index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+    connection = Faraday.new('https://last-airbender-api.herokuapp.com')
+
+    endpoint = '/api/v1/characters?affiliation=Fire+Nation'
+    response = connection.get(endpoint)
+    json = JSON.parse(response.body, symbolize_names: true)
+
+
   end
 end
